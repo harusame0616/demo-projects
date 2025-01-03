ba08894c072dc79db80bbc217c953418f229c5e7
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-03 21:15:47 +0900

    a を Link コンポーネントに修正

--- src/app/(user)/_components/post/attachment-presenter.tsx
+++ src/app/(user)/_components/post/attachment-presenter.tsx
@@ -1,14 +1,16 @@
 import { FileIcon } from "lucide-react";
 
+import { Link } from "@/components/link";
+
 type Props = {
   path: string;
   url: string;
 };
 export async function AttachmentPresenter({ path, url }: Props) {
   return (
-    <a href={url} className="flex items-center gap-1 underline-offset-1">
+    <Link href={url} className="flex items-center gap-1" prefetch={false} >
       <FileIcon className="size-4" />
       {path.split("/")[1]}
-    </a>
+    </Link>
   );
 }
