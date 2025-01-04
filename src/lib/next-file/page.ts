8eee1bd9681df8a60cf1d5667c4499d17396e8b9
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-04 17:18:40 +0900

    createPage の型改善

--- src/lib/next-file/page.ts
+++ src/lib/next-file/page.ts
@@ -4,21 +4,46 @@ import * as v from "valibot";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Schema = v.BaseSchema<any, any, any>;
 
+type Page<
+  SearchParamsSchema extends Schema | undefined,
+  ParamsSchema extends Schema | undefined,
+> = (props: {
+  searchParams: SearchParamsSchema extends undefined
+    ? undefined
+    : v.InferOutput<Exclude<SearchParamsSchema, undefined>>;
+  params: ParamsSchema extends undefined
+    ? undefined
+    : v.InferOutput<Exclude<ParamsSchema, undefined>>;
+  searchParamsRaw: Record<string, string | string[] | undefined>;
+  paramsRaw: Record<string, string>;
+}) => ReactNode;
+
+type NextPage = (props: {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+  params: Promise<Record<string, string>>;
+}) => Promise<ReactNode>;
+
+export function createPage(page: Page<undefined, undefined>): NextPage;
+export function createPage<SearchParamsSchema extends Schema>(
+  page: Page<SearchParamsSchema, undefined>,
+  option: { searchParamsSchema: SearchParamsSchema },
+): NextPage;
+export function createPage<ParamsSchema extends Schema>(
+  page: Page<undefined, ParamsSchema>,
+  option: { paramsSchema: ParamsSchema },
+): NextPage;
 export function createPage<
   SearchParamsSchema extends Schema,
   ParamsSchema extends Schema,
 >(
-  Page: (props: {
-    searchParams: v.InferOutput<SearchParamsSchema>;
-    params: v.InferOutput<ParamsSchema>;
-    searchParamsRaw: Record<string, string | string[] | undefined>;
-    paramsRaw: Record<string, string>;
-  }) => ReactNode,
-  option?: {
-    searchParamsSchema?: SearchParamsSchema;
-    paramsSchema?: ParamsSchema;
+  page: Page<SearchParamsSchema, ParamsSchema>,
+  option: {
+    searchParamsSchema: SearchParamsSchema;
+    paramsSchema: ParamsSchema;
   },
-) {
+): NextPage;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function createPage(page: any, option?: any) {
   return async function PageValidator({
     searchParams,
     params,
@@ -38,7 +63,7 @@ export function createPage<
       ? v.parse(option.paramsSchema, await params)
       : undefined;
 
-    return Page({
+    return page({
       searchParams: parsedSearchParams,
       params: parsedParams,
       searchParamsRaw: searchParamsAwaited,
