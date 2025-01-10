e698e5b418ebf0c38c5c3b95daba125993f36b35
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2025-01-10 21:47:29 +0900

    パスワード入力コンポーネントを作成してパスワードの内容を表示できるように変更 (#1)

---
+++ src/components/password-input.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { EyeClosedIcon, EyeIcon } from "lucide-react";
+import { ComponentProps, useState } from "react";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+type Props = ComponentProps<typeof Input>;
+export function PasswordInput(props: Props) {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="relative">
+      <Input type={visible ? "text" : "password"} {...props} className="pr-9" />
+      <div className="absolute inset-y-0 right-0">
+        <Button
+          size="icon"
+          variant="ghost"
+          type="button"
+          onClick={() => setVisible((prev) => !prev)}
+        >
+          {visible ? (
+            <EyeClosedIcon role="img" aria-label="to visible" />
+          ) : (
+            <EyeIcon role="img" aria-label="to invisible" />
+          )}
+        </Button>
+      </div>
+    </div>
+  );
+}
