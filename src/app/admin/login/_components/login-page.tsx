3c2f4b560cdff85091f8f88931b6d811aafb2478
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-10 21:00:47 +0900

    テスト修正

---
+++ src/app/admin/login/_components/login-page.tsx
@@ -0,0 +1,12 @@
+import { LoginCard } from "./login-card";
+
+export function LoginPage() {
+  return (
+    <div className="flex h-full flex-col items-center p-4">
+      <div className="max-h-[120px] grow" />
+      <div className="w-full max-w-sm">
+        <LoginCard />
+      </div>
+    </div>
+  );
+}
