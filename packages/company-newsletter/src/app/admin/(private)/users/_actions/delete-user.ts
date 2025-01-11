89c6c5042c01f0349244ad70ae441c465ddb3c05
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-12 00:02:19 +0900

    ユーザー関連の操作にユーザーテーブルをもとに変更

--- packages/company-newsletter/src/app/admin/(private)/users/_actions/delete-user.ts
+++ packages/company-newsletter/src/app/admin/(private)/users/_actions/delete-user.ts
@@ -1,3 +1,4 @@
+import { getPrismaClient } from "@/lib/prisma";
 import { fail, succeed } from "@/lib/result";
 import { createClient } from "@/lib/supabase/server";
 
@@ -5,9 +6,16 @@ type DeleteUserParams = { userId: string };
 export async function deleteUser({ userId }: DeleteUserParams) {
   const supabase = await createClient();
 
-  const result = await supabase.auth.admin.deleteUser(userId);
-
-  if (result.error) {
+  const prisma = getPrismaClient();
+  try {
+    await prisma.$transaction(async (tx) => {
+      await tx.cnlUser.delete({ where: { userId } });
+      const result = await supabase.auth.admin.deleteUser(userId);
+      if (result.error) {
+        throw result.error;
+      }
+    });
+  } catch {
     return fail("ユーザーの削除に失敗しました");
   }
 
